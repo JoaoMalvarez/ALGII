e1bae7609cb5944690be1d802b43fcd32fe7fc83
Joao-Pedro <joaopmalvarez@hotmail.com>
2025-09-02 08:28:39 -0300

    Atualizar o size_vetores.c

--- teste pessoal/size_vetores.c
+++ teste pessoal/size_vetores.c
@@ -5,10 +5,10 @@ int main() {
     //é utilizado para representar o tamanho de qualquer 
     //objeto em bytes, como resultado da operação sizeof
     size_t tam_vetor = sizeof(vetor);
-    printf ("Tamanho do vetor em bytes: %1d\n", tam_vetor);
+    printf ("Tamanho do vetor em bytes: %ld\n", tam_vetor);
     
     size_t tam_int = sizeof(int);
-    printf("Quantidade de bytes em um int: %1d\n", tam_int);
+    printf("Quantidade de bytes em um int: %ld\n", tam_int);
     
     size_t tam_elem = sizeof(vetor[0]);
     printf ("Tamanho de um elemento do vetor em bytes: %1d\n", tam_elem);
@@ -18,3 +18,26 @@ int main() {
     
     return 0;
 }
+
+
+
+-----------------------------
+
+#include <stdio.h›
+void funcao(int arr[]) {
+    printf(" funcao(): \n");
+    printf(" - tamanho do vetor: %ld\n", sizeof(arr));
+    printf(" - elementos do vetor: %ld\n", (sizeof (arr) / sizeof(arr[0])));
+}
+
+int main(){
+    printf ("tamanho de um ponteiro: %ld\n", sizeof(void*));
+    int vetor [] = ( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    printf("main(): \n");
+    printf(" - tamanho do vetor: %ld\n" sizeof (vetor));
+    printf(" - elementos do vetor: %ld\n", (sizeof(vetor) / sizeof (vetor [@])));
+    funcao (vetor);
+    return 0;
+}
+//O que será impresso?
+//Será impresso será a posição 8 
