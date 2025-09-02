ed58be1d7938b534ab9e18af216ad7e608bd7927
Joao-Pedro <joaopmalvarez@hotmail.com>
2025-09-02 08:32:46 -0300

    Atualizar o vetores.c

--- teste pessoal/vetores.c
+++ teste pessoal/vetores.c
@@ -14,3 +14,26 @@ int main() {
     printf("%d", vetor[i]);
     return 0;
 }
+
+
+--------------------------
+
+
+#include <stdio.h›
+
+//Função para somar todos os elementos de um vetor
+int somaVetor (int vetor(], int tamanho) {
+    int soma = 0;
+    for (int i = 0; i ‹ tamanho; i++) { 
+        soma += vetor [i];
+    }
+    return soma;
+}
+
+int main () {
+    int vetor [5] = (1, 2, 3, 4, 5};
+// Calcula e imprime a soma dos elementos do vetor
+    int soma = somaVetor (vetor, 5);
+    printf("Soma dos elementos: %d\n", soma);
+    return 0;
+}
